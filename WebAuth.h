c1183ad4143f518d30bf24506d5fb15c9d201ae3
Darguon <158139399+Darguon@users.noreply.github.com>
2025-04-07 22:30:51 +0300

    Add files via upload

---
+++ WebAuth.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Web-based authentication and loading system for HuhtalaHook
+class WebAuth
+{
+public:
+    // Starts the web authentication server and process
+    // Returns true if authentication is successful
+    static bool StartWebAuth();
+};
